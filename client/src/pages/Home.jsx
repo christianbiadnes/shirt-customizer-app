7f424c8821117789c2d934ff7e8e0d70d185913c
Christian Biadnes <cfbiadnes@gmail.com>
2023-10-05 22:05:09 +0800

    Home and other components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
 };
 
 export default Home;
